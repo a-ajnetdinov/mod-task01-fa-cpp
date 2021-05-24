52716c01fd0d61a1012d940eb44ca1fdc4f09da5
Anton Ajnetdinov <57314412+a-ajnetdinov@users.noreply.github.com>
2021-05-24 19:25:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+    cout << " 1. String: fwr r4 4 f da3de2 \n    Result: " << faStr1("fwr r4 4 f da3de2 ") << endl;
+    cout << " 2. String: Arr weSw 5asd A#da Asff2d AdsfGa\n    Result: " << faStr2("Arr weSw 5asd A#da Asff2d AdsfGa") << endl;
+    cout << " 3. String: Arr weSw 5asd A#da Asff2d AdsfGa\n    Result: " << faStr3("Arr weSw 5asd A#da Asff2d AdsfGa") << endl;
     return 0;
 }
